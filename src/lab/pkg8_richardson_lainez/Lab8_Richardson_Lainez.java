8e5b66b3af491bd3b9d03ecb0c5daa9d35e615f3
RALC365 <alejandro.carcamo@unitec.edu>
2017-12-08 11:51:19 -0600

    Primer Commit

---
+++ src/lab/pkg8_richardson_lainez/Lab8_Richardson_Lainez.java
@@ -0,0 +1,13 @@
+package lab.pkg8_richardson_lainez;
+
+/**
+ *
+ * @author RALC
+ */
+public class Lab8_Richardson_Lainez {
+    public static void main(String[] args) {
+    
+    
+    }
+    
+}
